e21e14187e9c4542ddc5b7e9fd9041d001c652fa
Lucas Dias <maykonlucasdias@gmail.com>
2023-03-29 14:32:26 -0300

    created menu method and logic

---
+++ ControleDeAtividades.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class ControleDeAtividades {
+  Scanner sc = new Scanner(System.in);
+  public void inicia() {
+    boolean controle = true;
+    while(controle) {
+      int escolha = mostraMenu();
+      
+      switch (escolha) {
+        case 1:
+          controle = cadastro();
+          break;
+        case 2:
+          controle = pesquisar();
+          break;
+        case 3:
+          controle = atualizar();
+          break;
+        case 4:
+          controle = remover();
+          break;
+        case 5:
+          controle = testar();
+          break;
+        case 6:
+          controle = false;
+          break;
+      }
+    }
+  }
+  
+  public int mostraMenu() {
+    List<Integer> opcoes = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+    
+    System.out.println("DIÁRIO DE ATIVIDADES\n----------------------------------------------");
+    System.out.println("1- Cadastrar Atividade\n2- Pesquisar Atividade\n3- Atualizar Atividade\n4- Remover Atividade\n5- Testar Programa\n6- Sair");
+    
+    while(true){
+      try {
+        System.out.print("Insira o número correspondente à sua escolha: ");
+        int escolha = Integer.parseInt(sc.nextLine());
+        if (!opcoes.contains(escolha)) {
+          throw new Exception();
+        }
+        return escolha;
+      } catch (Exception e) {
+        System.err.println("Insira um valor válido");
+      }
+    }
+  }
+}
--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
 public class Main {
-  
+  public static void main(String[] args) {
+    ControleDeAtividades controle = new ControleDeAtividades();
+    controle.inicia();
+  }
 }
